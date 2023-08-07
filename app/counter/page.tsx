1a1bdb113c7e967717283da30121e7bae9c1f956
Yuta Yamamori <ymyuta@amazon.co.jp>
2023-08-07 17:23:10 +0900

    Mod: change button style in the counter page

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -6,16 +6,18 @@ const Counter = () => {
   const [count, setCount] = useState<number>(0)
 
   return (
-    <>
-      <p>You clicked {count} times</p>
+    <div className="text-center m-16">
+      <div className="text-3xl">You clicked {count} times</div>
       <button
         onClick={() => {
           setCount(count + 1)
         }}
+        type="button"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xl px-5 py-1.5 mt-2"
       >
         Click me
       </button>
-    </>
+    </div>
   )
 }
 
